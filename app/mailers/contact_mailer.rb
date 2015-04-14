5d745ebd3c909b5f7edc38eb4a3ff852799c89aa
sergio sebastian arriagada <sergio@nibyru.com>
2015-04-13 22:23:26 -0300

    fix email contact

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -2,6 +2,6 @@ class ContactMailer < ActionMailer::Base
 	def new_contact(contact)
 	    @contact = contact
 	    puts ">>>>>>> send email ..."
-	    mail(to: "dotestudiografico@gmail.com", subject: 'New Contact')
+	    mail(from: @contact.email, to: "dotestudiografico@gmail.com", subject: 'New Contact')
 	end
 end
