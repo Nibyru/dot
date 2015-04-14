2d10014667da533bac3e8940db9684ab427ed1ad
sergio sebastian arriagada <sergio@nibyru.com>
2015-04-13 22:04:39 -0300

    add sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,9 +81,9 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address:              'smtp.sendgrid.net',
     port:                 587,
-    domain:               'gmail.com',
-    user_name:            'sarriagada',
-    password:             'meisner1051',
-    authentication:       'plain',
+    domain:               'heroku.com',
+    user_name:            ENV['SENDGRID_USERNAME'],
+    password:             ENV['SENDGRID_PASSWORD'],
+    authentication:       :plain,
     enable_starttls_auto: true  }
 end
